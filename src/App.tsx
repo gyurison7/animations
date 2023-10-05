49b6d5d0f7eb554fe16576ce16936c5f7c2fc78e
gyurison7 <orangetree401@gmail.com>
2023-10-06 01:21:32 +0900

    feat: slider animation

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
+
+const Wrapper = styled(motion.div)`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Box = styled(motion.div)`
+  width: 400px;
+  height: 200px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
+  position: absolute;
+  top: 100px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants = {
+  initial: (back:boolean) => ({ x: back ? -500 : 500, opacity: 0, scale: 0 }),
+  center: { x: 0, opacity: 1, scale: 1, transition: { duration: 0.5 } },
+  exit: (back:boolean) => ({ x: back ? 500: -500, opacity: 0, scale: 0, transition: { duration: 0.5 } }),
+};
+
+function App() {
+  const [visible, setVisible] = useState(1);
+  const [back, setBack] = useState(false);
+  const next = () => {
+    setBack(false);
+    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  };
+  const prev = () => {
+    setBack(true);
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  };
+  return (
+    <Wrapper>
+      <AnimatePresence custom={back}>
+        <Box
+          custom={back}
+          variants={boxVariants}
+          initial="initial"
+          animate="center"
+          exit="exit"
+          key={visible}
+        >
+          {visible}
+        </Box>
+      </AnimatePresence>
+      <button onClick={next}>next</button>
+      <button onClick={prev}>prev</button>
+    </Wrapper>
+  );
+}
+
+export default App;
