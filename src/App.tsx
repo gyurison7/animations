24d06b1a60f1bd8103087ee6620bd2771fe62675
gyurison7 <orangetree401@gmail.com>
2023-10-06 02:23:48 +0900

    feat: layout animation

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,19 @@ const Wrapper = styled(motion.div)`
   //flex-direction: column;
 `;
 
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+
 const Box = styled(motion.div)`
-  width: 400px;
-  height: 400px;
+  height: 200px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   display: flex;
@@ -22,21 +32,45 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  background-color: #00a5ff;
-  width: 100px;
-  height: 100px;
-  border-radius: 50%;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+// const Circle = styled(motion.div)`
+//   background-color: #00a5ff;
+//   width: 100px;
+//   height: 100px;
+//   border-radius: 50%;
+//   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+// `;
+
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 function App() {
-  const [click, setClick] = useState(false);
-  const toggleClick = () => setClick((prev) => !prev);
+  const [clickId, setClickId] = useState<string | null>(null);
   return (
-    <Wrapper onClick={toggleClick}>
-      <Box>{!click ? <Circle layoutId="circle" /> : null}</Box>
-      <Box>{click ? <Circle layoutId="circle" /> : null}</Box>
+    <Wrapper>
+      <Grid>
+        {["1", "2", "3", "4"].map((n) => (
+          <Box onClick={() => setClickId(n)} key={n} layoutId={n} />
+        ))}
+      </Grid>
+      <AnimatePresence>
+        {clickId ? (
+          <Overlay
+            onClick={() => setClickId(null)}
+            initial={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+            animate={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+            exit={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+          >
+            <Box layoutId={clickId} style={{ width: "400px", height: "200px" }} />
+          </Overlay>
+        ) : null}
+      </AnimatePresence>
     </Wrapper>
   );
 }
